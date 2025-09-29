ff42459d475ab91cb5574c4fb153c958b440e096
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-09-29 04:09:03 +0300

    Update 23_Code.jsx

--- 23_Code.jsx
+++ 23_Code.jsx
@@ -10,6 +10,25 @@ function MyApp() {
     </>
   );
 }
+export default MyApp;
 
+// ************** another function **********************
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About Page</h1>
+      <p>
+        this is the about <br /> page here it is. It is a simple page.
+      </p>
+    </>
+  );
+}
+
+function MyApp() {
+  return <AboutPage />;
+}
 
 export default MyApp;
+
+
