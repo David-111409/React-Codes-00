c3c59d2a8fe16b236f60f75845bcfe5a3838cc60
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-09-29 04:05:02 +0300

    Create 23_Code.jsx

---
+++ 23_Code.jsx
@@ -0,0 +1,15 @@
+function MyButton() {
+  return <button>I am a button</button>;
+}
+
+function MyApp() {
+  return (
+    <>
+      <h1>Welcome to My App</h1>
+      <MyButton />
+    </>
+  );
+}
+
+
+export default MyApp;
