472bff9996c8e2800a4519cd8f1720cf1d5e7178
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-09-29 16:45:26 +0300

    Create 24_Code.jsx

---
+++ 24_Code.jsx
@@ -0,0 +1,44 @@
+const user = {
+  name: "Hedy Lamarr",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  imageSize: 90,
+};
+
+function MyApp() {
+  let logged = true;
+  // if (logged) {
+  //   return (
+  //     <>
+  //       <h1>{user.name}</h1>
+  //       <img
+  //         src={user.imageUrl}
+  //         alt={"Photo of " + user.name}
+  //         style={{
+  //           height: user.imageSize,
+  //           width: user.imageSize,
+  //           borderRadius: "50%",
+  //         }}
+  //       />
+  //     </>
+  //   );
+  // } else return <h1>Please Log in with Acccount</h1>;
+//   return logged ? (
+//     <>
+//       <h1>{user.name}</h1>
+//       <img
+//         src={user.imageUrl}
+//         alt={"Photo of " + user.name}
+//         style={{
+//           height: user.imageSize,
+//           width: user.imageSize,
+//           borderRadius: "50%",
+//         }}
+//       />
+//     </>
+//   ) : (
+//     <h1>hello guest</h1>
+//   );
+ return logged && <h1>Your are an old user.</h1>
+}
+
+export default MyApp;
