17fceb7925301b66790cd32207c079b4bcdceaf0
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-09-30 21:13:27 +0300

    Update 26_Code.jsx

--- 26_Code.jsx
+++ 26_Code.jsx
@@ -18,3 +18,48 @@ function App() {
 }
 
 export default App;
+
+/*
+function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      alt="Katherine Johnson"
+      style={{ marginRight: "20px", borderRadius: "50%" }}
+    />
+  );
+}
+
+function App() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
+
+export default App;
+*/
+/*
+export default function TodoList() {
+  return (
+    <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        className="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve spectrum technology</li>
+      </ul>
+    </>
+  );
+}
+
+*/
