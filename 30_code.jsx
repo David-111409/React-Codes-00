e222f908b9dc821297ebdc69e13e798d840fa817
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-21 06:50:49 +0300

    Create 30_code.jsx

---
+++ 30_code.jsx
@@ -0,0 +1,19 @@
+// import { useState } from "react";
+// function App() {
+//   const [isChecked, setIsChecked] = useState(false);
+// //   console.log(isChecked);
+//   return (
+//     <>
+//       <label htmlFor="name">Name:</label>
+//       <input
+//         checked={isChecked}
+//         onChange={(e) => setIsChecked(e.target.checked)}
+//         type="checkbox"
+//         id="name"
+//         name="name"
+//       />
+//     </>
+//   );
+// }
+
+// export default App;
