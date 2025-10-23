eac711b9fc2bc06999b87df3388f2d8360e87f48
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-23 10:22:08 +0300

    Update 33_code.js

--- 33_code.js
+++ 33_code.js
@@ -1,36 +1,54 @@
-// import { useState } from "react";
+import { useState } from "react";
 
-// function App() {
-//   const [form, setForm] = useState({ name: "", email: "", age: "" });
-//   return (
-//     <form
-//       onSubmit={(e) => {
-//         e.preventDefault();
-//         console.log(form);
-//       }}
-//     >
-//       <h1>Form</h1>
-//       <input
-//         value={form.name}
-//         onChange={(e) => setForm({ ...form, name: e.target.value })}
-//         type="text"
-//         placeholder="Name"
-//       />
-//       <input
-//         value={form.email}
-//         onChange={(e) => setForm({ ...form, email: e.target.value })}
-//         type="text"
-//         placeholder="Email"
-//       />
-//       <input
-//         value={form.age}
-//         onChange={(e) => setForm({ ...form, age: e.target.value })}
-//         type="text"
-//         placeholder="Age"
-//       />
-//       <button type="submit">submit</button>
-//     </form>
-//   );
-// }
+function App() {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    age: "",
+    area: "",
+    isStudent: false,
+  });
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log(form);
+      }}
+    >
+      <h1>Form</h1>
+      <input
+        value={form.name}
+        onChange={(e) => setForm({ ...form, name: e.target.value })}
+        type="text"
+        placeholder="Name"
+      />
+      <input
+        value={form.email}
+        onChange={(e) => setForm({ ...form, email: e.target.value })}
+        type="text"
+        placeholder="Email"
+      />
+      <input
+        value={form.age}
+        onChange={(e) => setForm({ ...form, age: e.target.value })}
+        type="text"
+        placeholder="Age"
+      />
+      <textarea
+        value={form.area}
+        onChange={(e) => setForm({ ...form, area: e.target.value })}
+        placeholder="Area"
+      />
+      <label htmlFor="check">Are you a student?</label>
+      <input
+        id="check"
+        type="checkbox"
+        checked={form.isStudent}
+        onChange={(e) => setForm({ ...form, isStudent: e.target.checked })}
+      />
+      <button type="submit">submit</button>
+    </form>
+  );
+}
 
-// export default App;
+export default App;
