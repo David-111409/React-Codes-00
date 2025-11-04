9a9338aa56c24cd3c908dbd9692b9218d410c20a
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-11-04 21:43:48 +0200

    Create 43_code.jsx

---
+++ 43_code.jsx
@@ -0,0 +1,121 @@
+import { useState } from "react";
+import { places } from "./data.js";
+import { getImageUrl } from "./utils.js";
+import { createContext, useContext } from "react";
+export const ImageContext = createContext();
+
+export default function App() {
+  const [isLarge, setIsLarge] = useState(false);
+  const imageSize = isLarge ? 200 : 80;
+  return (
+    <>
+      <label>
+        <input
+          type="checkbox"
+          checked={isLarge}
+          onChange={(e) => {
+            setIsLarge(e.target.checked);
+          }}
+        />
+        Use large images
+      </label>
+      <hr />
+      <ImageContext.Provider value={imageSize}>
+        <List />
+      </ImageContext.Provider>
+    </>
+  );
+}
+
+function List() {
+  const listItems = places.map((place) => (
+    <li key={place.id}>
+      <Place place={place}  />
+    </li>
+  ));
+  return <ul>{listItems}</ul>;
+}
+
+function Place({ place }) {
+  
+  return (
+    <>
+      <PlaceImage place={place}  />
+      <p>
+        <b>{place.name}</b>
+        {": " + place.description}
+      </p>
+    </>
+  );
+}
+
+function PlaceImage({ place }) {
+  const useImageSize = useContext(ImageContext);
+  return (
+    <img
+      src={getImageUrl(place)}
+      alt={place.name}
+      width={useImageSize}
+      height={useImageSize}
+    />
+  );
+}
+
+// data.js
+export const places = [
+  {
+    id: 0,
+    name: "Bo-Kaap in Cape Town, South Africa",
+    description:
+      "The tradition of choosing bright colors for houses began in the late 20th century.",
+    imageId: "K9HVAGH",
+  },
+  {
+    id: 1,
+    name: "Rainbow Village in Taichung, Taiwan",
+    description:
+      "To save the houses from demolition, Huang Yung-Fu, a local resident, painted all 1,200 of them in 1924.",
+    imageId: "9EAYZrt",
+  },
+  {
+    id: 2,
+    name: "Macromural de Pachuca, Mexico",
+    description:
+      "One of the largest murals in the world covering homes in a hillside neighborhood.",
+    imageId: "DgXHVwu",
+  },
+  {
+    id: 3,
+    name: "Selarón Staircase in Rio de Janeiro, Brazil",
+    description:
+      'This landmark was created by Jorge Selarón, a Chilean-born artist, as a "tribute to the Brazilian people."',
+    imageId: "aeO3rpI",
+  },
+  {
+    id: 4,
+    name: "Burano, Italy",
+    description:
+      "The houses are painted following a specific color system dating back to 16th century.",
+    imageId: "kxsph5C",
+  },
+  {
+    id: 5,
+    name: "Chefchaouen, Marocco",
+    description:
+      "There are a few theories on why the houses are painted blue, including that the color repels mosquitos or that it symbolizes sky and heaven.",
+    imageId: "rTqKo46",
+  },
+  {
+    id: 6,
+    name: "Gamcheon Culture Village in Busan, South Korea",
+    description:
+      "In 2009, the village was converted into a cultural hub by painting the houses and featuring exhibitions and art installations.",
+    imageId: "ZfQOOzf",
+  },
+];
+
+
+// utils.js
+export function getImageUrl(place) {
+  return "https://i.imgur.com/" + place.imageId + "l.jpg";
+}
