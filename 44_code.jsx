a8ec5db9cf5c18dea1283d10bc886b5e5e7aec8d
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-11-07 04:38:52 +0200

    Update 44_code.jsx

--- 44_code.jsx
+++ 44_code.jsx
@@ -30,3 +30,70 @@ function Content() {
   );
 }
 export default App;
+/*
+import { useState } from "react";
+function App() {
+  // const [word, setWord] = useState("");
+  // const [words, setWords] = useState([]);
+  const [name, setName] = useState("");
+  const [text, setText] = useState("");
+  const [choosen, setChoosen] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(name);
+  }
+  return (
+    <>
+      <h1>The Form part 1</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setName(e.target.value)}
+          type="text"
+          placeholder="Enter your name"
+        />
+        <textarea value={text} onChange={(e) => setText(e.target.value)} />
+          <select value = {choosen} onChange={(e) => setChoosen(e.target.value)}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+        <button>Submit</button>
+      </form>
+      <h2>Name : {name}</h2>
+      <h3>Text area : {text} and the <br/> choosen : {choosen}</h3>
+    </>
+  );
+
+  /* <div className="App">
+      { <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          fetch("https://api.datamuse.com/words?rel_syn=fast")
+            .then((res) => res.json())
+            .then(setWords)
+            .catch((err) => console.error(err));
+        }}
+      >
+        <label htmlFor="word">Your Word</label>
+        <input
+          onChange={(e) => setWord(e.target.value)}
+          value={word}
+          id="word"
+        />
+        <button>Submit</button>
+        <ul>
+          {words.map((word) => (
+            <li onClick={(e) => setWord(word.word)} key={crypto.randomUUID()}>
+              {word.word}
+            </li>
+          ))}
+        </ul>
+      </form>
+    </div> }
+
+}
+
+export default App;
+
+*/
+    */
