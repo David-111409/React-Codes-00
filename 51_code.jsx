9eccfdc76e3c23162f64adc511cbe14d9f4465cb
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-11-30 02:28:14 +0200

    Update 51_code.jsx

--- 51_code.jsx
+++ 51_code.jsx
@@ -23,3 +23,32 @@ function App() {
 }
 
 export default App;
+/*
+import { useState } from "react";
+
+export default function RequestTracker() {
+    const [pending, setPending] = useState(0);
+    const [completed, setCompleted] = useState(0);
+
+    async function handleClick() {
+        setPending((prev) => prev + 1);
+        await delay(3000);
+        setPending((prev) => prev - 1);
+        setCompleted((prev) => prev + 1);
+    }
+
+    return (
+        <>
+            <h3>Pending: {pending}</h3>
+            <h3>Completed: {completed}</h3>
+            <button onClick={handleClick}>Buy</button>
+        </>
+    );
+}
+
+function delay(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+*/
